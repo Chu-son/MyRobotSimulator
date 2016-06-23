0b017940580282fe9fd5fcc34674cac62071047b
KosukeNakamura <s15mh133@mail.saitama-u.ac.jp>
2016-06-24 02:29:39 +0900

    up

---
+++ Assets/Scripts/RobotScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotScript : MonoBehaviour {
+    public float transformSpeed;
+    public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("right"))
+        {
+            transform.eulerAngles += new Vector3(0f, rotateSpeed, 0f);
+        }
+        else if (Input.GetKey("left"))
+        {
+            transform.eulerAngles += new Vector3(0f, -rotateSpeed, 0f);
+        }
+        else if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * transformSpeed;
+        }
+        else if (Input.GetKey("down"))
+        {
+            transform.position += transform.forward * -transformSpeed;
+        }
+	
+	}
+}
